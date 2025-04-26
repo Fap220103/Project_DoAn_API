b20fbe72180b90604289d83a369a76a7c2b241e4
LQTrung05 <lqtrungfcb@gmail.com>
2025-04-26 16:18:35 +0700

    add

--- Application/Features/AddressOrder/Queries/GetShippingAddress.cs
+++ Application/Features/AddressOrder/Queries/GetShippingAddress.cs
@@ -1,4 +1,5 @@
-﻿using Application.Services.CQS.Queries;
+﻿using Application.Common.Models;
+using Application.Services.CQS.Queries;
 using Domain.Entities;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
@@ -12,13 +13,15 @@ namespace Application.Features.AddressOrder.Queries
 {
     public class GetShippingAddressResult
     {
-        public IEnumerable<ShippingAddress> Data { get; set; } = null!;
+        public PagedList<ShippingAddress> Data { get; set; } = null!;
         public string Message { get; init; } = null!;
     }
 
     public class GetShippingAddressRequest : IRequest<GetShippingAddressResult>
     {
         public string userId { get; set; }
+        public int Page { get; set; } = 1;
+        public int Limit { get; set; } = 10;
     }
 
     public class GetShippingAddressHandler : IRequestHandler<GetShippingAddressRequest, GetShippingAddressResult>
@@ -34,11 +37,20 @@ namespace Application.Features.AddressOrder.Queries
 
         public async Task<GetShippingAddressResult> Handle(GetShippingAddressRequest request, CancellationToken cancellationToken)
         {
-            var entities = await _context.ShippingAddress.Where(x=> x.UserId == request.userId).ToListAsync(cancellationToken);
+            var query = _context.ShippingAddress.Where(x=> x.UserId == request.userId).AsQueryable();
+            // Phân trang
+            var skip = (request.Page - 1) * request.Limit;
+            var items = await query
+                .Skip(skip)
+                .Take(request.Limit)
+                .ToListAsync(cancellationToken);
 
+
+            var total = await query.CountAsync(cancellationToken);
+            var pagedList = new PagedList<ShippingAddress>(items, total, request.Page, request.Limit);
             return new GetShippingAddressResult
             {
-                Data = entities,
+                Data = pagedList,
                 Message = "Success"
             };
         }
